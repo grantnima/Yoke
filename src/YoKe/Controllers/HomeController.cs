5d101277ca849f4ed2d9b6949877122ea48dba4e
Grant <1093536815@qq.com>
2017-05-06 16:16:54 +0800

    上传Yoke代购平台
    使用MVC.CORE框架搭建电子商务平台

---
+++ src/YoKe/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YoKe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+        public IActionResult PersonalCenter()
+        {
+            
+            return View();
+        }
+        public IActionResult Message()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/YoKe/wwwroot/js/indext.js
@@ -0,0 +1,14 @@
+﻿script.type = "text/javascript";
+script.src = "jq/jquery-3.1.0.js";
+function cartsjia() {
+    var valus1 = $('#carttext').val();
+    var valus2 = parseInt(valus1) + 1;
+    $('#carttext').val(valus2);
+    $('#carttext1').val(valus2);
+}
+function cartsjian() {
+    var valus1 = $('#carttext').val();
+    var valus2 = parseInt(valus1) - 1;
+    $('#carttext').val(valus2);
+    $('#carttext1').val(valus2);
+}
