653a1ad00e45b1e89ae5f3506c9c8fa19599f9c7
Grant <1093536815@qq.com>
2017-05-26 15:29:49 +0800

    完善购物车功能

--- src/YoKe/Controllers/CartController.cs
+++ src/YoKe/Controllers/CartController.cs
@@ -5,12 +5,7 @@ using YoKe.Models;
 using Microsoft.AspNetCore.Http;
 using YoKe.Infrastructure;
 
-
-
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace YoKe.Controllers
+namespace FlowerWorld.Controllers
 {
     public class CartController : Controller
     {
@@ -19,14 +14,27 @@ namespace YoKe.Controllers
         {
             db = _db;
         }
-        // GET: /<controller>/
+        //
+        // GET: /Cart/
         public ActionResult Index()
         {
+
+            if (Request.Query["retUrl"].ToString() != "")
+            {
+                ViewBag.continueBuy = Request.Query["retUrl"].ToString();
+            }
+            else
+            {
+                ViewBag.continueBuy = Request.Headers["Referer"].ToString();
+            }
+            ViewBag.contBuy = ViewBag.continueBuy;
+
             List<int[]> curCart = HttpContext.Session.GetJson<List<int[]>>("Cart");
-            
+            List<int[]> curFavi = HttpContext.Session.GetJson<List<int[]>>("Favi");
             if (curCart == null) curCart = new List<int[]>();
-            
+            if (curFavi == null) curFavi = new List<int[]>();
             List<CartItem> cart = new List<CartItem>();
+            List<CartItem> favi = new List<CartItem>();
             foreach (int[] i in curCart)
             {
                 int curId = i[0];
@@ -39,14 +47,32 @@ namespace YoKe.Controllers
                                              productName = p.ProductName,
                                              
                                              price = (double)p.Price,
-                                               
                                              qty = curQty,
                                              
                                          }).FirstOrDefault<CartItem>();
                 cart.Add(cartItem);
             }
-
+            foreach (int[] i in curFavi)
+            {
+                int curId = i[0];
+                int curQty = i[1];
+                CartItem cartItem = (from p in db.Product
+                                     where p.ObjId == curId
+                                     select
+                                         new CartItem
+                                         {
+                                             productName = p.ProductName,
+                                             
+                                             price = (double)p.Price,
+                                             qty = curQty,
+                                             
+                                         }).FirstOrDefault<CartItem>();
+                favi.Add(cartItem);
+            }
+            
             ViewBag.cart = cart;
+            ViewBag.favi = favi;
+           
             return View("Cart");
         }
 
@@ -73,9 +99,87 @@ namespace YoKe.Controllers
                 }
                 HttpContext.Session.SetJson("Cart", curCart);
             }
-
-
             return Index();
+        }
+
+        public RedirectResult AddFavi(int id)
+        {
+            List<int[]> curFavi = HttpContext.Session.GetJson<List<int[]>>("Favi");
+            if (curFavi == null)
+                HttpContext.Session.SetJson("Favi", new List<int[]> { new int[] { id, 1 } });
+            else
+            {
+                bool found = false;
+                foreach (var p in curFavi)
+                {
+                    if (p[0] == id)
+                    {
+                        found = true;
+                        p[1] += 1;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    curFavi.Add(new int[] { id, 1 });
+                }
+                HttpContext.Session.SetJson("Favi", curFavi);
+            }
+
+            string continueBuy = Request.Headers["Referer"].ToString();
+            if (Request.Query["retUrl"].ToString() != "")
+            {
+                continueBuy = Request.Query["retUrl"].ToString();
+            }
+            return Redirect(continueBuy);
+        }
+
+        public RedirectResult updateCartRow(int id)
+        {
+            int value = int.Parse(Request.Query["value"].ToString());
+            List<int[]> curCart = HttpContext.Session.GetJson<List<int[]>>("Cart");
+            curCart[id][1] = value;
+            HttpContext.Session.SetJson("Cart", curCart);
+            return Redirect("/Cart?retUrl=" + Request.Query["retUrl"].ToString());
+        }
+        public RedirectResult deleCartRow(int id)
+        {
+            List<int[]> curCart = HttpContext.Session.GetJson<List<int[]>>("Cart");
+            curCart.RemoveAt(id);
+            HttpContext.Session.SetJson("Cart", curCart);
+            return Redirect("/Cart?retUrl=" + Request.Query["retUrl"].ToString());
+        }
+        public RedirectResult storeCartRow(int id)
+        {
+            List<int[]> curCart = HttpContext.Session.GetJson<List<int[]>>("Cart");
+            List<int[]> curFavi = HttpContext.Session.GetJson<List<int[]>>("Favi");
+            if (curFavi == null)
+            {
+                curFavi = new List<int[]>();
+                HttpContext.Session.SetJson("Favi", curFavi);
+            }
+            curFavi.Add(curCart[id]);
+            curCart.RemoveAt(id);
+            HttpContext.Session.SetJson("Cart", curCart);
+            HttpContext.Session.SetJson("Favi", curFavi);
+            return Redirect("/Cart?retUrl=" + Request.Query["retUrl"].ToString());
+        }
+        public RedirectResult deleFaviRow(int id)
+        {
+            List<int[]> curFavi = HttpContext.Session.GetJson<List<int[]>>("Favi");
+            curFavi.RemoveAt(id);
+            HttpContext.Session.SetJson("Favi", curFavi);
+            return Redirect("/Cart?retUrl=" + Request.Query["retUrl"].ToString());
+        }
+        public RedirectResult buyFaviRow(int id)
+        {
+            List<int[]> curCart = HttpContext.Session.GetJson<List<int[]>>("Cart");
+            List<int[]> curFavi = HttpContext.Session.GetJson<List<int[]>>("Favi");
+            curCart.Add(curFavi[id]);
+            curFavi.RemoveAt(id);
+            HttpContext.Session.SetJson("Cart", curCart);
+            HttpContext.Session.SetJson("Favi", curFavi);
+            return Redirect("/Cart?retUrl=" + Request.Query["retUrl"].ToString());
         }
     }
 }
